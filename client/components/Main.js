16be6ed8a9990a3332fc202b11561580a0ef721b
FourEyeSamurai <leeson.chris@gmail.com>
2017-07-14 15:37:02 -0500

    Fixed initial errors

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,2 +1,16 @@
+import React, { Component } from 'react';
 
+class Main extends Component {
+ 
+  render() {
+    return (
+      <div> 
+       <p>HI</p>
+      </div>
+    )
+  }
+
+}
+
+export default Main;
 
